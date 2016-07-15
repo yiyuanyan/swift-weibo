a8051d60d5b1080b2cbcdfbf3638cfcc229363e9
何建新 <yiyuanhanly@126.com>
2016-07-15 17:04:51 +0800

    第六天该第三个视频了

--- swift-weibo/swift-weibo/Classes/Module/Home/HomeViewController.swift
+++ swift-weibo/swift-weibo/Classes/Module/Home/HomeViewController.swift
@@ -7,9 +7,8 @@
 //
 
 import UIKit
-
 class HomeViewController: BaseTableViewController {
-
+    var rowHeightCache = NSCache();
     @IBOutlet weak var titleButton: HomtTitleButton!
     //当这个数据被赋值后调用方法
     var statuses:[Status]?{
@@ -60,7 +59,28 @@ class HomeViewController: BaseTableViewController {
         // #warning Incomplete implementation, return the number of sections
         return 1;
     }
-
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath:NSIndexPath) -> CGFloat{
+        //使用微博ID作为KEY进行存取
+        
+        let status = statuses![indexPath.row];
+        //判断是否已经有缓存的行高
+        if (rowHeightCache.objectForKey(status.id) != nil){
+            print("返回缓存行高");
+            return rowHeightCache.objectForKey(status.id) as! CGFloat;
+        }
+        //当没有渠道缓存行高的时候
+        let cell = tableView.dequeueReusableCellWithIdentifier(StatusCell.cellIdentifer(status)) as! StatusCell;
+        let height = cell.rowHeight(status);
+        //保存行高到cache
+        rowHeightCache.setObject(height, forKey: status.id);
+        print("计算行高:\(height)");
+        return height;
+    }
+    //预估行高
+    override func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 300;
+    }
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         //快速判断是否为空，为空则返回0
@@ -68,11 +88,13 @@ class HomeViewController: BaseTableViewController {
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("HomeCell")! as! StatusCell;
+        let status = statuses![indexPath.row];
+        let cell = tableView.dequeueReusableCellWithIdentifier(StatusCell.cellIdentifer(status))! as! StatusCell;
         cell.status = statuses![indexPath.row];
         
         return cell;
     }
+
     
 
 }
--- swift-weibo/swift-weibo/Classes/Module/Home/StatusCell.swift
+++ swift-weibo/swift-weibo/Classes/Module/Home/StatusCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class StatusCell: UITableViewCell {
 
+    @IBOutlet weak var recontentLabel: UILabel!
+    @IBOutlet weak var bottomView: UIView!
     @IBOutlet weak var pictureViewHeight: NSLayoutConstraint!
     
     @IBOutlet weak var pictureViewWidth: NSLayoutConstraint!
@@ -38,6 +40,11 @@ class StatusCell: UITableViewCell {
             pictureViewHeight.constant = p.viewSize.height;
             pictureViewWidth.constant = p.viewSize.width;
             pictureViewLayout.itemSize = p.itemSize;
+            //recontentLabel.text = ("@" + (status?.retweeted_status?.user?.name)! ?? "") + ":" + (status?.retweeted_status?.text ?? "");
+            
+            if(status?.retweeted_status?.text != nil){
+                recontentLabel.text = "@" + (status?.retweeted_status?.user?.name)! + ":" + (status?.retweeted_status?.text)!;
+            }
             pictureView.reloadData();
         }
     }
@@ -48,7 +55,7 @@ class StatusCell: UITableViewCell {
         let m:CGFloat = 10;
         let imageCount = status.pictureURLs?.count ?? 0;
         if imageCount == 0 {
-            return (CGSizeZero,CGSizeZero);
+            return (CGSizeZero,itemSize);
         }
         if imageCount == 1{
             //从缓存中获取图片
@@ -66,11 +73,29 @@ class StatusCell: UITableViewCell {
         let row = CGFloat((imageCount - 1)/3 + 1);
         return (CGSizeMake(s * 3 + m * 2, s * row + (row - 1) * m), itemSize);
     }
-    
+    func rowHeight(status: Status) -> CGFloat{
+        //设置微博数据，获取数据模型数据
+        self.status = status;
+        //设置cell的强制更新,设置后视图排版已完成
+        self.layoutIfNeeded();
+        
+        //返回最后一个view的Y
+        return CGRectGetMaxY(bottomView.frame);
+    }
+    //根据数据来获取准确可重用标识符
+    class func cellIdentifer(status:Status) -> String{
+        if(status.retweeted_status != nil){
+            return "RetweetedCell";
+        }
+        return "HomeCell";
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         //设置label的最大宽度值
         contentLabel.preferredMaxLayoutWidth = UIScreen.mainScreen().bounds.width - 20;
+        
+        
+            recontentLabel?.preferredMaxLayoutWidth = UIScreen.mainScreen().bounds.width - 20;
         
         // Initialization code
     }
